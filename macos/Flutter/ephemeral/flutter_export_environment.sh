139de73858672dd6b1724f6f1b1dd2ad6f081f16
Siguismund-291104 <oscarloyola206@gmail.com>
2025-12-01 09:32:30 -0600

    Add files via upload

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\oscar\develop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\oscar\OneDrive\Escritorio\Flutter\ecu3"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\dart projects\mi_receta"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gestor_alumnos/main.dart';
+
+import 'package:mi_receta/main.dart';
 
 void main() {
-  testWidgets('La aplicación inicia correctamente',
-      (WidgetTester tester) async {
-    // Build de la app
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MiRecetaApp());
 
-    // Verifica que la pantalla de inicio cargó
-    expect(find.text('Gestor de Alumnos'), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-    // Verifica que el formulario está presente
-    expect(find.text('Nombre'), findsOneWidget);
-    expect(find.text('Carrera'), findsOneWidget);
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
